ee7268dacf9eedead1048eaf3e5c84cdf473fa05
powcoder <powcoder@163.com>
2022-11-21 15:00:26 +0800

    CMPSC 461 HW2 Scheme

---
+++ code.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+;p1 
+(define funPower "TODO") 
+
+(define encode "TODO")
+
+(define decode "TODO")
+
+(define MULT "TODO")
+
+
+;p2
+(define dot "TODO")
+
+;p3
+(define in "TODO")
+
+;p4
+(define lstOddSum "TODO")
+
+;p5
+(define trunc "TODO")
+
+(define leq "TODO")
+
+(define dup "TODO")
+
